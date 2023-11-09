5188085bd77314325f893f797d94a30d8ffe562c
rafittu <rafittu@outlook.com>
2023-11-09 18:32:55 -0300

    feat: setup app main

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,31 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import * as swaggerUi from 'swagger-ui-express';
+import * as SwaggerDoc from '../swagger.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
+  app.use(helmet());
+  app.use(helmet.hidePoweredBy());
+  app.use(helmet.contentSecurityPolicy());
+
+  app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(SwaggerDoc));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   app.enableShutdownHooks();
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT, () => console.log('WoPhi!'));
 }
 bootstrap();
