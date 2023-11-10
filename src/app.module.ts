a2d8806d7515d7a8a9cb43a1a3c7fa1fd80c0e88
rafittu <rafittu@outlook.com>
2023-11-10 17:48:16 -0300

    feat: .env schema validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { PrismaService } from './modules/prisma.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3001),
+        DATABASE_URL: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_HOST_CONTAINER: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [],
   providers: [PrismaService],
 })
