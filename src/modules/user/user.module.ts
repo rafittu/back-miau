2141e8eed9ab1de238d7eeeb68766f43351e2671
rafittu <rafittu@outlook.com>
2023-11-13 16:24:34 -0300

    feat: set user repository as provider

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { CreateAdminUserService } from './services/user-admin.service';
+import { UserRepository } from './repository/user.repository';
 
 @Module({
   controllers: [UserController],
-  providers: [CreateAdminUserService],
+  providers: [UserRepository, CreateAdminUserService],
 })
 export class UserModule {}
