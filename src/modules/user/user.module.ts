da66ca242d58583de7ebfef9547e3102c63162bf
rafittu <rafittu@outlook.com>
2023-11-13 16:59:02 -0300

    fix: set prisma service as provider

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { CreateAdminUserService } from './services/user-admin.service';
 import { UserRepository } from './repository/user.repository';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserRepository, CreateAdminUserService],
+  providers: [PrismaService, UserRepository, CreateAdminUserService],
 })
 export class UserModule {}
