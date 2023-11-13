828d0620de5d34c46842ed4d28bfffd5f9a1b8fe
rafittu <rafittu@outlook.com>
2023-11-13 15:53:10 -0300

    feat: create admin controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,14 +1,21 @@
-import { Controller, UseFilters } from '@nestjs/common';
+import { Body, Controller, Post, UseFilters } from '@nestjs/common';
 import { HttpExceptionFilter } from '../../common/filter/http-exception.filter';
 import { AppError } from '../../common/errors/Error';
+import { isPublic } from '../../common/infra/decorators/is-public.decorator';
+import { CreateUserDto } from './dto/create-user.dto';
+import { CreateAdminUserService } from './services/user-admin.service';
 
 @Controller('user')
 @UseFilters(new HttpExceptionFilter(new AppError()))
 export class UserController {
-  // @Post()
-  // create(@Body() createUserDto: CreateUserDto) {
-  //   return this.userService.create(createUserDto);
-  // }
+  constructor(private readonly adminUserService: CreateAdminUserService) {}
+
+  @isPublic()
+  @Post('/admin')
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.adminUserService.execute(createUserDto);
+  }
+
   // @Get()
   // findAll() {
   //   return this.userService.findAll();
