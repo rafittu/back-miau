f03511d68c5652499d7286eeae3cfe02d3bb494b
rafittu <rafittu@outlook.com>
2023-11-13 16:05:01 -0300

    feat: set admin service as provider

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
+import { CreateAdminUserService } from './services/user-admin.service';
 
 @Module({
   controllers: [UserController],
-  providers: [],
+  providers: [CreateAdminUserService],
 })
 export class UserModule {}
