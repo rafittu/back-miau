865cec93f67efe0b11751f5d67dba67fc211ba5a
rafittu <rafittu@outlook.com>
2023-11-13 18:56:10 -0300

    test: create admin controller successfully

--- src/modules/user/test/user.controller.spec.ts
+++ src/modules/user/test/user.controller.spec.ts
@@ -1,10 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from '../user.controller';
 import { CreateAdminUserService } from '../services/user-admin.service';
-import { mockPrismaEmployee } from './mocks/user.controller.mock';
+import {
+  mockCreateUserBody,
+  mockPrismaEmployee,
+} from './mocks/user.controller.mock';
 
 describe('UserController', () => {
   let controller: UserController;
+
+  let createAdminService: CreateAdminUserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -20,9 +25,32 @@ describe('UserController', () => {
     }).compile();
 
     controller = module.get<UserController>(UserController);
+
+    createAdminService = module.get<CreateAdminUserService>(
+      CreateAdminUserService,
+    );
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create admin user', () => {
+    it('should create a new one successfully', async () => {
+      const result = await controller.createAdmin(mockCreateUserBody);
+
+      expect(createAdminService.execute).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(mockPrismaEmployee);
+    });
+
+    // it('should throw an error', async () => {
+    //   jest
+    //     .spyOn(createAdminService, 'execute')
+    //     .mockRejectedValueOnce(new Error());
+
+    //   await expect(
+    //     controller.createAdminUser(mockCreateUserBody),
+    //   ).rejects.toThrowError();
+    // });
   });
 });
