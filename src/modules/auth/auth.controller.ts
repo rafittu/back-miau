2f9af0bc16338fb11b6bfd5ce1d30a3da3593de1
rafittu <rafittu@outlook.com>
2023-11-14 14:40:17 -0300

    feat: signin endpoint controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,18 +1,18 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  Request,
-} from '@nestjs/common';
+import { Controller, Post, Request, UseFilters } from '@nestjs/common';
+import { AppError } from '../../common/errors/Error';
+import { HttpExceptionFilter } from '../../common/filter/http-exception.filter';
+import { isPublic } from '../../common/infra/decorators/is-public.decorator';
+import { SignInService } from './services/signin.service';
 
 @Controller('auth')
+@UseFilters(new HttpExceptionFilter(new AppError()))
 export class AuthController {
-  @Post()
+  constructor(private readonly signInService: SignInService) {}
+
+  @isPublic()
+  @Post('/signin')
   signIn(@Request() req) {
-    return 'return jwt token';
+    const { user } = req;
+    return this.signInService.execute(user);
   }
 }
