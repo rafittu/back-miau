57915313e7b13a2dd98d0bd95025dab80cc675a5
rafittu <rafittu@outlook.com>
2023-11-14 15:29:53 -0300

    feat: add signin local guard

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Post, Request, UseFilters } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Request,
+  UseFilters,
+  UseGuards,
+} from '@nestjs/common';
 import { AppError } from '../../common/errors/Error';
 import { HttpExceptionFilter } from '../../common/filter/http-exception.filter';
 import { isPublic } from './infra/decorators/is-public.decorator';
 import { SignInService } from './services/signin.service';
 import { AuthRequest, UserToken } from './interfaces/services.interface';
+import { LocalAuthGuard } from './infra/guards/local-auth.guard';
 
 @Controller('auth')
 @UseFilters(new HttpExceptionFilter(new AppError()))
@@ -12,6 +19,7 @@ export class AuthController {
 
   @isPublic()
   @Post('/signin')
+  @UseGuards(LocalAuthGuard)
   signIn(@Request() req: AuthRequest): UserToken {
     const { user } = req;
     return this.signInService.execute(user);
