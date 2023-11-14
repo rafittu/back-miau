b9bd91ac65cb57786c2e4fff8296995471f487bd
rafittu <rafittu@outlook.com>
2023-11-14 14:53:56 -0300

    label: sign in types

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { AppError } from '../../common/errors/Error';
 import { HttpExceptionFilter } from '../../common/filter/http-exception.filter';
 import { isPublic } from '../../common/infra/decorators/is-public.decorator';
 import { SignInService } from './services/signin.service';
+import { AuthRequest, UserToken } from './interfaces/services.interface';
 
 @Controller('auth')
 @UseFilters(new HttpExceptionFilter(new AppError()))
@@ -11,7 +12,7 @@ export class AuthController {
 
   @isPublic()
   @Post('/signin')
-  signIn(@Request() req) {
+  signIn(@Request() req: AuthRequest): UserToken {
     const { user } = req;
     return this.signInService.execute(user);
   }
--- src/modules/auth/interfaces/services.interface.ts
+++ src/modules/auth/interfaces/services.interface.ts
@@ -1,3 +1,9 @@
+import { Request } from 'express';
+
+export class AuthRequest extends Request {
+  user: UserPayload;
+}
+
 export interface UserPayload {
   id: string;
   username: string;
