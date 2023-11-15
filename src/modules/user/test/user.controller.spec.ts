3d8f12ffe348cdc4d8ec58966ff4b27cf6d4cbe3
rafittu <rafittu@outlook.com>
2023-11-15 18:07:58 -0300

    test: create employee user successfully

--- src/modules/user/test/user.controller.spec.ts
+++ src/modules/user/test/user.controller.spec.ts
@@ -5,11 +5,13 @@ import {
   mockCreateUserBody,
   mockPrismaEmployee,
 } from './mocks/user.module.mock';
+import { CreateEmployeeUserService } from '../services/employee-user.service';
 
 describe('UserController', () => {
   let controller: UserController;
 
   let createAdminService: CreateAdminUserService;
+  let createEmployeeService: CreateEmployeeUserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -21,6 +23,12 @@ describe('UserController', () => {
             execute: jest.fn().mockResolvedValue(mockPrismaEmployee),
           },
         },
+        {
+          provide: CreateEmployeeUserService,
+          useValue: {
+            execute: jest.fn().mockResolvedValue(mockPrismaEmployee),
+          },
+        },
       ],
     }).compile();
 
@@ -28,6 +36,9 @@ describe('UserController', () => {
 
     createAdminService = module.get<CreateAdminUserService>(
       CreateAdminUserService,
+    );
+    createEmployeeService = module.get<CreateEmployeeUserService>(
+      CreateEmployeeUserService,
     );
   });
 
@@ -51,6 +62,15 @@ describe('UserController', () => {
       await expect(
         controller.createAdmin(mockCreateUserBody),
       ).rejects.toThrow();
+    });
+  });
+
+  describe('create employee user', () => {
+    it('should create a new one successfully', async () => {
+      const result = await controller.createEmployee(mockCreateUserBody);
+
+      expect(createEmployeeService.execute).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(mockPrismaEmployee);
     });
   });
 });
