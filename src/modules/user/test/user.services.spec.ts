49a0c49db185c1213518fcd5636becc161621ec3
rafittu <rafittu@outlook.com>
2023-11-15 18:03:51 -0300

    test: create employee app error cases

--- src/modules/user/test/user.services.spec.ts
+++ src/modules/user/test/user.services.spec.ts
@@ -144,5 +144,33 @@ describe('UserServices', () => {
         expect(error.code).toBe(400);
       }
     });
+
+    it('should throw an App Error', async () => {
+      jest
+        .spyOn(userRepository, 'createUser')
+        .mockRejectedValueOnce(
+          new AppError('error.code', 401, 'Error message'),
+        );
+
+      try {
+        await createEmployeeService.execute(mockCreateUserBody);
+      } catch (error) {
+        expect(error).toBeInstanceOf(AppError);
+        expect(error.code).toBe(401);
+      }
+    });
+
+    it('should throw an error', async () => {
+      jest
+        .spyOn(userRepository, 'createUser')
+        .mockRejectedValueOnce(new Error());
+
+      try {
+        await createEmployeeService.execute(mockCreateUserBody);
+      } catch (error) {
+        expect(error).toBeInstanceOf(AppError);
+        expect(error.code).toBe(400);
+      }
+    });
   });
 });
