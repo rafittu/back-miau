b66fb30c29b3d33b0a168ad4e28487970317dbf9
rafittu <rafittu@outlook.com>
2023-11-15 17:07:52 -0300

    feat: update providers list

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,9 +3,15 @@ import { UserController } from './user.controller';
 import { CreateAdminUserService } from './services/admin-user.service';
 import { UserRepository } from './repository/user.repository';
 import { PrismaService } from 'src/prisma.service';
+import { CreateEmployeeUserService } from './services/employee-user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [PrismaService, UserRepository, CreateAdminUserService],
+  providers: [
+    PrismaService,
+    UserRepository,
+    CreateAdminUserService,
+    CreateEmployeeUserService,
+  ],
 })
 export class UserModule {}
