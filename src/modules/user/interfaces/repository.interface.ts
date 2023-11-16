c52fb71de49ef4054a5a9470b9bcc3668fe41c19
rafittu <rafittu@outlook.com>
2023-11-16 15:31:21 -0300

    feat: create function to format user return

--- src/modules/user/interfaces/repository.interface.ts
+++ src/modules/user/interfaces/repository.interface.ts
@@ -1,7 +1,21 @@
-import { EmployeeInfo } from '@prisma/client';
+import { EmployeeInfo, EmployeeRole, EmployeeStatus } from '@prisma/client';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UserRole } from '../enum/user-role.enum';
 
 export interface IUserRepository {
   createUser(data: CreateUserDto, role: UserRole): Promise<EmployeeInfo>;
+}
+
+export interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+  phone: string;
+  position: string;
+  role: EmployeeRole;
+  status: EmployeeStatus;
+  createdAt: Date;
+  updatedAt: Date;
 }
--- src/modules/user/repository/user.repository.ts
+++ src/modules/user/repository/user.repository.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { EmployeeInfo, Prisma } from '@prisma/client';
 import { PrismaService } from '../../../prisma.service';
 import { AppError } from '../../../common/errors/Error';
-import { IUserRepository } from '../interfaces/repository.interface';
+import { IUserRepository, User } from '../interfaces/repository.interface';
 import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UserRole } from '../enum/user-role.enum';
@@ -10,6 +10,36 @@ import { UserRole } from '../enum/user-role.enum';
 @Injectable()
 export class UserRepository implements IUserRepository {
   constructor(private prisma: PrismaService) {}
+
+  private formatUserReturn(employeeData: EmployeeInfo): User {
+    const {
+      id,
+      first_name,
+      last_name,
+      username,
+      email,
+      phone,
+      position,
+      role,
+      status,
+      created_at,
+      updated_at,
+    } = employeeData;
+
+    return {
+      id,
+      firstName: first_name,
+      lastName: last_name,
+      username,
+      email,
+      phone,
+      position,
+      role,
+      status,
+      createdAt: created_at,
+      updatedAt: updated_at,
+    };
+  }
 
   async createUser(data: CreateUserDto, role: UserRole): Promise<EmployeeInfo> {
     const {
@@ -44,6 +74,8 @@ export class UserRepository implements IUserRepository {
       });
 
       delete user.password;
+
+      console.log(user);
 
       return user;
     } catch (error) {
