94a4a9e444bc8736c8514edfd0fae8bc9a4140fc
rafittu <rafittu@outlook.com>
2024-06-18 14:44:03 -0300

    chore: update module imports

--- src/modules/employee/employee.module.ts
+++ src/modules/employee/employee.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { EmployeeController } from './employee.controller';
 import { PrismaService } from 'src/prisma.service';
+import { AlmaService } from 'src/common/api/alma.service';
+import { EmployeeRepository } from './repository/employee.repository';
+import { CreateAdminService } from './services/admin-user.service';
 
 @Module({
   controllers: [EmployeeController],
-  providers: [PrismaService],
+  providers: [
+    PrismaService,
+    AlmaService,
+    EmployeeRepository,
+    CreateAdminService,
+  ],
 })
 export class EmployeeModule {}
