73f5260ce1fd023a707e3ee6997286adf6d22fae
rafittu <rafittu@outlook.com>
2024-06-18 17:59:34 -0300

    feat: setup employee controller test

--- src/modules/employee/tests/employee.controller.spec.ts
+++ src/modules/employee/tests/employee.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { EmployeeController } from './employee.controller';
-import { EmployeeService } from './employee.service';
+import { EmployeeController } from '../employee.controller';
+import { CreateAdminService } from '../services/admin-user.service';
 
 describe('EmployeeController', () => {
   let controller: EmployeeController;
@@ -8,7 +8,14 @@ describe('EmployeeController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [EmployeeController],
-      providers: [EmployeeService],
+      providers: [
+        {
+          provide: CreateAdminService,
+          useValue: {
+            execute: jest.fn().mockResolvedValue(''),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<EmployeeController>(EmployeeController);
