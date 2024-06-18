295ab8128ab8931e566c46e14b0f944804b59e25
rafittu <rafittu@outlook.com>
2024-06-18 18:17:38 -0300

    test: create admin user controller

--- src/modules/employee/tests/employee.controller.spec.ts
+++ src/modules/employee/tests/employee.controller.spec.ts
@@ -1,9 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EmployeeController } from '../employee.controller';
 import { CreateAdminService } from '../services/admin-user.service';
+import {
+  MockCreateEmployeeDto,
+  MockICreateEmployee,
+} from './mocks/employee.mock';
 
 describe('EmployeeController', () => {
   let controller: EmployeeController;
+
+  let createAdminService: CreateAdminService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,16 +18,27 @@ describe('EmployeeController', () => {
         {
           provide: CreateAdminService,
           useValue: {
-            execute: jest.fn().mockResolvedValue(''),
+            execute: jest.fn().mockResolvedValue(MockICreateEmployee),
           },
         },
       ],
     }).compile();
 
     controller = module.get<EmployeeController>(EmployeeController);
+
+    createAdminService = module.get<CreateAdminService>(CreateAdminService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create admin user', () => {
+    it('should create a new admin user successfully', async () => {
+      const result = await controller.createAdmin(MockCreateEmployeeDto);
+
+      expect(createAdminService.execute).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(MockICreateEmployee);
+    });
   });
 });
