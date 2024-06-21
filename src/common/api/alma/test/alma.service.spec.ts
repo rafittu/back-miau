62b447758b1830f847df0027298b493942274350
rafittu <rafittu@outlook.com>
2024-06-21 12:05:38 -0300

    test: external api post request

--- src/common/api/alma/test/alma.service.spec.ts
+++ src/common/api/alma/test/alma.service.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AlmaService } from '../alma.service';
+import axios from 'axios';
+import { MockCreateEmployeeDto } from '../../../../modules/employee/tests/mocks/employee.mock';
+
+jest.mock('axios');
 
 describe('ALMA external api', () => {
   let almaService: AlmaService;
@@ -14,5 +18,37 @@ describe('ALMA external api', () => {
 
   it('should be defined', () => {
     expect(almaService).toBeDefined();
+  });
+
+  describe('handle alma api requests', () => {
+    it('should make a post request and return response data', async () => {
+      const mockCreateUserAxiosResponse = {
+        data: { fakeUser: 'user data' },
+      };
+
+      (
+        axios.post as jest.MockedFunction<typeof axios.post>
+      ).mockResolvedValueOnce(mockCreateUserAxiosResponse);
+
+      const path = 'example.com/api';
+
+      const result = await almaService['almaRequest'](
+        path,
+        'post',
+        undefined,
+        MockCreateEmployeeDto,
+      );
+
+      expect(axios.post).toHaveBeenCalledWith(
+        path,
+        MockCreateEmployeeDto,
+        expect.objectContaining({
+          headers: {
+            Authorization: `Bearer ${undefined}`,
+          },
+        }),
+      );
+      expect(result).toEqual(mockCreateUserAxiosResponse.data);
+    });
   });
 });
